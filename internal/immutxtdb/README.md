95805644162514147f3b858948621b3a7c2d397f
mxBossard <mxbossard@gmail.com>
2026-05-18 23:16:00 +0200

    Added a dummy zip method.

--- internal/immutxtdb/README.md
+++ internal/immutxtdb/README.md
@@ -41,8 +41,9 @@
       - [x] Test Bucket Commit Then Get Then Update then Get
       - [x] Test Commit Get Update on different partitions (with conflicts)
       - [x] Return an error on bucket.Project() if conflict.
-- [_] For now use a Symbolic Zip & Encrypt methode: "Zip[<DATA>] ; Encrypt[<DATA>] for test purpose.
+- [_] For now use a Symbolic Zip method: "Zip[<DATA>] for test purpose.
       - [_] Check bucket service files contents (notably for multiple imports)
+- [_] For now use a Symbolic Encrypt methode: "Encrypt[<DATA>] for test purpose.
 - [_] Add Store / Service Snapshot() HOW ?
 - [_] Add idx export / import unittests (especially with hashed keys)
 - [_] Rework idx Encoders => seq, state & time MUST be encoded by an internal encoder (not responsability of idx user)
--- internal/immutxtdb/zip/zip.go
+++ internal/immutxtdb/zip/zip.go
@@ -3,15 +3,26 @@ package zip
 import "fmt"
 
 const (
+	SymZip              = byte(0)
 	ZlibCompressionMark = byte(1)
 )
 
+// const zipMethod = ZlibCompressText
+const zipMethod = SymZip
+
 func ZipString(txt string) ([]byte, error) {
-	ziped, err := ZlibCompressText(txt)
+	var zipFunc func(string) ([]byte, error)
+	switch zipMethod {
+	case SymZip:
+		zipFunc = symCompressText
+	case ZlibCompressionMark:
+		zipFunc = ZlibCompressText
+	}
+	ziped, err := zipFunc(txt)
 	if err != nil {
 		return nil, err
 	}
-	return append([]byte{ZlibCompressionMark}, ziped...), nil
+	return append([]byte{zipMethod}, ziped...), nil
 }
 
 func UnzipString(data []byte) (string, error) {
@@ -19,9 +30,19 @@ func UnzipString(data []byte) (string, error) {
 		return "", fmt.Errorf("no data to unzip")
 	}
 	switch data[0] {
+	case SymZip:
+		return symDecompressText(data[1:])
 	case ZlibCompressionMark:
 		return ZlibDecompressText(data[1:])
 	default:
 		return "", fmt.Errorf("not supported compression algorithm")
 	}
+}
+
+func symCompressText(txt string) ([]byte, error) {
+	return []byte("Zip[" + txt + "]"), nil
+}
+
+func symDecompressText(data []byte) (string, error) {
+	return string(data[4 : len(data)-1]), nil
 }
